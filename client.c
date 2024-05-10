675df4aac57e6a41b5a250974f8521c477c03cfc
Giacomo Maldarella <giacomomaldarella@gmail.com>
2024-05-10 19:49:37 +0200

    updated latest version

--- client.c
+++ client.c
@@ -7,7 +7,60 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>        
 #include "utility.h"
+
+int MAX_LENGTH = 1024;
+/*void wait_command(){
+    printf("> ");
+    fflush(stdout);
+}*/
+
+int start(){
+    int choice;
+    
+    printf("Choose:\n");
+    printf("[1] Registration\n");
+    printf("[2] Login\n");
+
+    
+    while(1){
+        printf("> ");
+        scanf("%d", &choice);
+        while(getchar() !='\n');
+        if(choice != 1 && choice != 2){
+            printf("Scelta non valida\n");
+        }else{
+            return choice;
+        }
+    }
+
+}
+
+void registration(char email[],char username[],char password[]){
+    //inserire credenziali
+    printf("Inserisci le tue credenziali\n");
+    printf("email: ");
+    fgets(email, MAX_LENGTH, stdin); // Leggo una riga da tastiera
+    printf("username: ");  
+    fgets(username, MAX_LENGTH, stdin); // Leggo una riga da tastiera
+    printf("password: ");  
+    fgets(password, MAX_LENGTH, stdin); // Leggo una riga da tastiera
+   
+    //generazione chiave pub associata legata allo username
+
+
+    // mandare handshake
+    //handshake();
+    // mandare msg server
+    // generare credenziali
+    // mandare credenziali cifrate e con firma
+    // numero causuale
+}
+
+void handshake(){
+
+}
 
 void help(){
     printf("[1] List(int n): lists the latest n available messages in the BBS");
@@ -38,7 +91,12 @@ int main(int argc, char** argv){
     uint16_t lmsg;
     int clt_port;
     struct sockaddr_in srv_addr;
-    char msg[50];
+    char msg[MAX_LENGTH];
+    int var=0;
+    char email[MAX_LENGTH];
+    char username[MAX_LENGTH];
+    char password[MAX_LENGTH];
+
 
     if (argc != 2){
         printf("Parametri non validi, usa: ./dev <porta>\n");
@@ -48,6 +106,9 @@ int main(int argc, char** argv){
     
     clt_port = atoi(argv[1]);
 
+    // generazione chiave pubblica
+    
+    var = start();
     sd = socket(AF_INET,SOCK_STREAM,0);
 
     memset(&srv_addr, 0, sizeof(srv_addr));
@@ -61,7 +122,12 @@ int main(int argc, char** argv){
         perror("Errore in fase di connessione: \n");
         exit(1);
     }
-    
+
+    if(var == 1){
+        registration(email, username, password);
+    }else{
+        //login();
+    }
     
     printf("Inserisci un messaggio: ");
     fgets(msg, sizeof(msg), stdin); 
